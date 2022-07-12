843552955b5775df6f7749f33d1b010973fb4ec2
Joshua Kempter <80900819+DerKempter@users.noreply.github.com>
2022-07-12 20:52:30 +0000

    update everything

--- DBHandler.py
+++ DBHandler.py
@@ -2,71 +2,126 @@ from replit import db
 import mwclient
 
 class DBHandler():
-  def __init__(self):
-    self.keys = db.keys()
-    self.champs = self.grab_all_champions()
+		def __init__(self):
+				self.keys = db.keys()
+				self.champs = self.grab_all_champions()
 
-  def grab_all_champions(self) -> []:
-    site = mwclient.Site('lol.fandom.com', path='/')
-    query_response = site.api('cargoquery', limit="max", tables="Champions=Ch", fields="Ch.Name")
-    champs = [champ_dict['title']['Name'] for champ_dict in query_response['cargoquery']]
+		def grab_all_champions(self) -> []:
+				site = mwclient.Site('lol.fandom.com', path='/')
+				query_response = site.api('cargoquery', limit="max", tables="Champions=Ch", fields="Ch.Name")
+				champs = [champ_dict['title']['Name'] for champ_dict in query_response['cargoquery']]
 
-    return champs
+				return champs
 
-  def check_if_champ_is_finished(self, userId: int, champId: int) -> bool:
-    value = self.get_key_value(userId)
-    if value['champs'][str(champId)]['finished']:
-      return True
-    else:
-      return False
+		def check_if_champ_is_finished(self, userId: int, champId: int) -> bool:
+				value = self.get_key_value(userId)
+				if value['champs'][str(champId)]['finished']:
+						return True
+				else:
+						return False
 
-  def add_key_value_pair(self, key: str, value: str):
-    db[key] = value
+		def add_key_value_pair(self, key: str, value: str):
+				db[key] = value
 
-  def remove_key_value_pair(self, key: str):
-    if self.check_if_key_exists(key):
-      del db[str(key)]
-      return True
-    else:
-      return False
+		def remove_key_value_pair(self, key: str):
+				if self.check_if_key_exists(key):
+						del db[str(key)]
+						return True
+				else:
+						return False
 
-  def get_key_value(self, key: str):
-    if self.check_if_key_exists(key):
-      return db[str(key)]
-    else: 
-      return False
+		def get_key_value(self, key: str):
+				if self.check_if_key_exists(key):
+						return db[str(key)]
+				else: 
+						return False
 
-  def check_if_key_exists(self, key: str):
-    result = True
-    try:
-      _ = db[str(key)]
-    except KeyError:
-      result = False
-    return result
+		def generate_json_dummy(self, userId: str, isAtoZ: bool):
+				dummy = {"userId": str(userId), "isAtoZ": False, "currentChamp": 0, "champs": {} }
+				if isAtoZ:
+						dummy["isAtoZ"] = True
+						dummy["currentChamp"] = 1
+				else:
+						dummy["isAtoZ"] = False
+						dummy["currentChamp"] = len(self.champs)
 
-  def generate_json_dummy(self, userId: str, isAtoZ: bool):
-    
-    dummy = {
-    "userId": str(userId),
-    "isAtoZ": False,
-    "currentChamp": 0,
-    "champs": {}
-    }
-    if isAtoZ:
-      dummy["isAtoZ"] = True
-      dummy["currentChamp"] = 1
-    else:
-      dummy["isAtoZ"] = False
-      dummy["currentChamp"] = len(self.champs)
+				champs_dict = {}
+				for champion in self.champs:
+						temp_dict = {"tries": 0, "finished": False}
+						champs_dict[self.champs.index(champion) + 1] = temp_dict
+				dummy["champs"] = champs_dict
 
-    champs_dict = {}
-    for champion in self.champs:
-      temp_dict = {
-        "tries": 0,
-        "finished": False
-      }
-      champs_dict[self.champs.index(champion) + 1] = temp_dict
+				return dummy
 
-    dummy["champs"] = champs_dict
+		def check_if_key_exists(self, key: str):
+				try:
+						_ = db[str(key)]
+				except KeyError:
+						return False
+				return True
+		
+		def is_user_finished(self, userId):
+				userEntry = db[str(userId)]['champs']
+				for value in userEntry:
+						champDict = userEntry[value]
+						print(champDict)
+						if champDict['finished'] == False:
+								return False
+				return True
 
-    return dummy
+		def grab_player_stats(self, user):
+				userId = user.id
+				base_str_1 = ""
+				base_str_2 = ""
+				base_str_3 = ""
+				done_str = ""
+				started_str = ""
+				done_champs = []
+				started_champs = []
+				userEntry = db[str(userId)]['champs']
+				for value in userEntry:
+						champDict = userEntry[value]
+						if champDict['finished'] == True:
+								done_champs.append((self.champs[int(value) - 1], champDict['tries']))
+						elif champDict['tries'] > 0:
+								started_champs.append((self.champs[int(value) - 1], champDict['tries']))
+				base_str_1 += f"Stats for {user.name} \n"
+				base_str_2 += f"{user.name} has won on the following champions: \n"
+
+				for championName, tries in done_champs:
+						done_str += f"{championName} in {tries} attemps. \n"
+
+				base_str_3 += f"{user.name} has played the following champs unsuccessfully: \n"
+
+				for championName, tries in started_champs:
+						started_str += f"{championName} with currently {tries} attemps \n"
+
+				return base_str_1, base_str_2, done_str, base_str_3, started_str
+
+		def get_winrate_for_id(self, user):
+				userId = user.id
+				userEntry = db[str(userId)]['champs']
+				totalTries = 0
+				totalWins = 0
+				totalLosses = 0
+				champsPlayed = 0
+				for value in userEntry:
+						champDict = userEntry[value]
+						if champDict['finished'] == True:
+								champTries = champDict['tries']
+								totalTries += champTries
+								totalWins += 1
+								totalLosses = champTries - 1
+								champsPlayed += 1
+						elif champDict['tries'] > 0:
+								champTries = champDict['tries']
+								totalTries += champTries
+								totalLosses += champTries
+								champsPlayed += 1
+				if totalTries == 0:
+						return
+				winrate = totalWins / totalTries * 100
+				str_1 = f"You have had a total of {totalTries} Attempts at a total of {champsPlayed} Champions"
+				str_2 = f"Your winrate accros all those games is {winrate}%"
+				return str_1, str_2
+							
--- main_old.py
+++ main_old.py
@@ -1,6 +1,11 @@
 import os
-from interactions import CommandContext, Option
-import interactions
+from threading import Thread
+from time import sleep
+
+from discord.ext import commands
+from discord.commands import Option
+from discord.ui import Button, Select
+from discord import SelectOption
 
 import mwclient
 
@@ -8,91 +13,69 @@ from DBHandler import DBHandler
 
 bot_token = os.environ['TOKEN']
 
-client = interactions.Client(bot_token)
+client = commands.Bot()
 
 dbHandler = DBHandler()
 guilds = [775065533518708738]
 
 
-@client.command(
-    name="start",
-    scope=guilds,
-    description="Beginn Challenge.",
-    options=[
-        Option(
-            name="order", 
-            type=interactions.OptionType.BOOLEAN, 
-            description="Order `A to Z` or `Z to A`",
-            required=False)
-    ])
-async def start(ctx: CommandContext, order: bool):
-    print(ctx)
+@client.slash_command(guild_ids=guilds, description="Beginn Challenge. ")
+async def start(ctx, order: Option(bool, "Order `A to Z` or `Z to A`")):
     userId = ctx.author.id
     if dbHandler.check_if_key_exists(userId):
-        await ctx.message(
+        await ctx.respond(
             f"You already registered for the Challenge. View your progress with /Stats"
         )
     else:
         userName = ctx.author.name
         dummy_dict = dbHandler.generate_json_dummy(userId, order)
         dbHandler.add_key_value_pair(userId, dummy_dict)
-        await ctx.send(f"user {userName} with Id {userId} has been saved!")
+        await ctx.respond(f"user {userName} with Id {userId} has been saved!")
 
 
-@client.command(
-    name="show_all_champions",
-    scope=guilds,
-    description="display all Champs in alphabetical order"
-)
-async def show_all_champions(ctx: CommandContext):
+@client.slash_command(guild_ids=guilds,
+                      description="display all Champs in alphabetical order")
+async def show_champs(ctx):
     champs = grab_all_champions()
     message_template = ""
     for champion in champs:
         message_template += champion
-    await ctx.send(message_template)
+    await ctx.respond(message_template)
 
 
-@client.command(
-    name="current",
-    scope=guilds, 
-    description="display current champ progression")
-async def current(ctx: CommandContext):
-    print(ctx)
+@client.slash_command(guild_ids=guilds,
+                      description="display current champ progression")
+async def current(ctx):
     userId = ctx.author.id
     if not dbHandler.check_if_key_exists(userId):
-        await ctx.send(
+        await ctx.respond(
             'You have not yet started the challenge. Do so with `/start`')
         return
     value = dbHandler.get_key_value(userId)
     currentChampId = value['currentChamp']
     currentChampTries = value['champs'][str(currentChampId)]['tries']
     currentChamp = dbHandler.champs[currentChampId - 1]
-    await ctx.send(
+    await ctx.respond(
         f"You're currently at {currentChamp} and have so far had {currentChampTries} Attemps at this Champion"
     )
 
 
-@client.command(
-    name="add_remove_attempts",
-    scope=guilds,
-    description="Add or remove one or multiple failed attempts to the current or one of the next champs",
-    options=[
-        Option(
-            name="amount",
-            type=interactions.OptionType.INTEGER,
-            description="the Amount of attempts to add or remove to the champion [default: 1]",
-            min_value=-10,
-            max_value=10,
-            required=False,
-            default=1),
-        Option(
-            name="skip",
-            type=interactions.OptionType.INTEGER,
-            description="The amount of champs to skip from the current [default: 0]",
-            required=False,
-            default=0)
-    ])
-async def add_remove_attempts(ctx: CommandContext, amount=1, skip=0):
+@client.slash_command(
+    guild_ids=guilds,
+    description=
+    "Add or remove one or multiple failed attempts to the current or one of the next champs"
+)
+async def add_remove_attempts(ctx, amount: Option(
+    int,
+    "the Amount of attempts to add or remove to the champion [default: 1]",
+    min_value=-10,
+    max_value=10,
+    required=False,
+    default=1), skip: Option(
+        int,
+        "The amount of champs to skip from the current [default: 0]",
+        required=False,
+        default=0)):
     value = dbHandler.get_key_value(ctx.author.id)
     currentChampId = value['currentChamp']
     actualChampId = value['currentChamp'] + skip
@@ -105,30 +88,23 @@ async def add_remove_attempts(ctx: CommandContext, amount=1, skip=0):
     skip = skip * AtoZ
 
     if skip != 1:
-        await ctx.send(
+        await ctx.respond(
             f"You've added another attempts to {actualChamp} and are now at {actualChampTries} Attempts at this Champion"
         )
-    await ctx.send(
+    await ctx.respond(
         f"You're currently at {currentChamp} and have now had {currentChampTries} Attemps at this Champion"
     )
     return
 
 
-@client.command(
-    name="finish_champion",
-    scope=guilds,
-    description="Add a final attempts to a champion and mark it as finished",
-    options=[
-        Option(
-            name="skip",
-            type=interactions.OptionType.INTEGER,
-            description="The amount of champs to skip from the current [default: 0]",
-            required=False,
-            default=0)
-    ])
-async def finish_champion(ctx: CommandContext, skip: int):
-    if skip is None:
-        skip = 0
+@client.slash_command(
+    guild_ids=guilds,
+    description="Add a final attempts to a champion and mark it as finished")
+async def finish_champion(ctx, skip: Option(
+    int,
+    "The amount of champs to skip from the current [default: 0]",
+    required=False,
+    default=0)):
     userId = ctx.author.id
     value = dbHandler.get_key_value(userId)
     currentChampId = value['currentChamp']
@@ -149,7 +125,7 @@ async def finish_champion(ctx: CommandContext, skip: int):
                                      userId)
         print(nextChampId, currentChampId)
     if nextChampId == 0 or nextChampId == len(dbHandler.champs) + 1:
-        await ctx.send('Congratulations! You have completed the challange!')
+        await ctx.respond('Congratulations! You have completed the challange!')
         return
     else:
         value['currentChamp'] = nextChampId
@@ -163,42 +139,30 @@ async def finish_champion(ctx: CommandContext, skip: int):
         f"Your new Champion is {nextChamp} with currently {nextChampTries} Attemps."
     )
 
-    await ctx.send(
+    await ctx.respond(
         f"You're have finished {actualChamp} with {actualChampTries} Attemps.")
-    await ctx.send(
+    await ctx.respond(
         f"Your new Champion is {nextChamp} with currently {nextChampTries} Attemps."
     )
 
 
-@client.command(
-    name="stats",
-    scope=guilds, 
-    description="List your current stats",
-    options=[
-        Option(
-            name="finished_champs",
-            type=interactions.OptionType.BOOLEAN,
-            description="show finished champions",
-            required=False,
-            default=False),
-        Option(
-            name="started_champs",
-            type=interactions.OptionType.BOOLEAN,
-            description="show started champions",
-            required=False,
-            default=True),
-        Option(
-            name="winrate",
-            type=interactions.OptionType.BOOLEAN,
-            description="show winrate across all games",
-            required=False,
-            default=True)
-    ])
-async def stats(ctx: CommandContext, done: bool, started: bool, winrate: bool):
+@client.slash_command(guild_ids=guilds, description="List your current stats")
+async def stats(ctx, done: Option(bool,
+                                  "show finished champions",
+                                  required=False,
+                                  default=False),
+                started: Option(bool,
+                                "show started champions",
+                                required=False,
+                                default=True),
+                winrate: Option(bool,
+                                "show winrate across all games",
+                                required=False,
+                                default=True)):
     base_str_1, base_str_2, done_str, base_str_3, started_str = dbHandler.grab_player_stats(
         ctx.author)
     print(base_str_1, done_str, base_str_2, started_str)
-    await ctx.send(base_str_1)
+    await ctx.respond(base_str_1)
     if done and done_str != "":
         await ctx.send(base_str_2)
         while len(done_str) > 0:
@@ -214,10 +178,24 @@ async def stats(ctx: CommandContext, done: bool, started: bool, winrate: bool):
         await ctx.send(winRate_str_2)
 
 
+# @client.slash_command(guild_ids=guilds, description="Add tries to specific champion")
+# async def add_tries_for_specific_champ(ctx):
+# 	champ_options = []
+# 	champs = grab_all_champions()
+# 	for champ in champs:
+# 			temp_option = SelectOption(label=champ, value=champ)
+# 			champ_options.append(temp_option)
+# 	champ_menu = Select(
+# 			custom_id="Champ_menu",
+# 			options=[]
+# 	)
+# 	await ctx.send("Select a champion", component=champ_menu, ephemeral=True)
+
+
 @client.event
 async def on_ready():
-		print(dbHandler.keys)
-		print(f'We have logged in as {client.me.name}')
+    print(dbHandler.keys)
+    print(f'We have logged in as {client.user}')
 
 
 @client.event
@@ -237,7 +215,7 @@ async def on_message(message):
             'You quit the challenge successfully you looser.')
 
 
-def get_next_champ(champs: list, AtoZ: bool, currentChamp: int, userId: int):
+def get_next_champ(champs: [], AtoZ: bool, currentChamp: int, userId: int):
     foundNewChamp = False
     print("testtest")
     if check_if_challenge_is_won(userId):
@@ -256,7 +234,7 @@ def get_next_champ(champs: list, AtoZ: bool, currentChamp: int, userId: int):
             print(f"{nextChampId=} = {currentChamp=} - 1")
 
 
-def grab_all_champions():
+def grab_all_champions() -> []:
     site = mwclient.Site('lol.fandom.com', path='/')
     query_response = site.api('cargoquery',
                               limit="max",
@@ -274,4 +252,4 @@ def grab_all_champions():
 def check_if_challenge_is_won(userId):
     return dbHandler.is_user_finished(userId)
 
-client.start()
+client.run(bot_token)
